862bdf88852d987d67aef498a6e1ba08a3579cb3
Richard Joyce <rjoyce@ucdavis.edu>
2017-02-23 17:55:20 -0800

    Add conda support and virtual env symbol

--- segments/virtual_env.py
+++ segments/virtual_env.py
@@ -1,11 +1,13 @@
 import os
 
+virtual_env_symbol = u'\ue73c'
+
 def add_virtual_env_segment(powerline):
-    env = os.getenv('VIRTUAL_ENV') or os.getenv('CONDA_ENV_PATH')
+    env = os.getenv('VIRTUAL_ENV') or os.getenv('CONDA_ENV_PATH') or os.getenv('CONDA_DEFAULT_ENV')
     if env is None:
         return
 
     env_name = os.path.basename(env)
     bg = Color.VIRTUAL_ENV_BG
     fg = Color.VIRTUAL_ENV_FG
-    powerline.append(' %s ' % env_name, fg, bg)
+    powerline.append(' {} {} '.format(virtual_env_symbol, env_name), fg, bg)
